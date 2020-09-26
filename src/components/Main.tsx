c2e6a501d8a40f4e0be40c270d014e3811b94313
Suh Donghwi <hwidongsuh@gmail.com>
2020-09-27 03:11:15 +0900

    feat: add floating box

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -13,13 +13,26 @@ const Container = styled.div`
 
 const MainContainer = styled.main`
   flex: 1;
+  display: flex;
+`;
+
+const FloatingBox = styled.section`
+  flex: 1;
+  margin: 4rem;
+  box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+  border-radius: 10px;
 `;
 
 const Main = () => (
   <Container>
     <Bar />
     <MainContainer>
-      <CodeEditor />
+      <FloatingBox>
+        <CodeEditor />
+      </FloatingBox>
+      <FloatingBox>
+        <CodeEditor />
+      </FloatingBox>
     </MainContainer>
   </Container>
 );
